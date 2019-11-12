08f44b42bb0aa99091d96977e6442e97d98dd48d
Marcleidson <47360687+Marcleidson@users.noreply.github.com>
2019-11-12 09:38:12 -0300

    Update imc.js

--- imc.js
+++ imc.js
@@ -1,3 +1,12 @@
 const nome = 'Carlos'
 const peso = 84
 const altura = 1.88
+
+imc = peso / (altura * altura)
+
+function calc () {
+  if (imc >= 30) {
+    console.log ('Carlos você está acima do peso.')
+  } else 
+    cosole.log ('Carlos você não está acima do peso')
+  }
